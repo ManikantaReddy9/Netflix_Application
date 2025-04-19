d74e3ce3c176b636810cba7494d27203a3ec2a92
ManikantaReddy9 <manikantareddymca@gmail.com>
2025-04-19 16:01:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NetflixUI from './NetflixUI'
+
+export default function App() {
+  return (
+    <div style={{textAlign:"center"}}>
+      <NetflixUI />
+    </div>
+  )
+}
---
+++ src/NetflixUI.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NetflixUI() {
+  return (
+    <div>
+      <h2>Netflix</h2>
+    </div>
+  )
+}
